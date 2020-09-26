a8990b57c2665a8655167956ad62011c6d83331d
engysamy29 <48186100+engysamy29@users.noreply.github.com>
2020-09-26 17:30:02 +0200

    Add files via upload

---
+++ TinderSwipeInterface/TinderStack/TinderStack/Model/CardsDataModel.swift
@@ -0,0 +1,16 @@
+import UIKit
+struct CardsDataModel {
+    
+    var bgColor: UIColor
+    var text : String
+    var image : String
+      
+    init(bgColor: UIColor, text: String, image: String) {
+        self.bgColor = bgColor
+        self.text = text
+        self.image = image
+    
+    }
+}
+
+
